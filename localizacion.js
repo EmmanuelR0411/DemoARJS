d62a539224ffb63fec99481461c7d47165097ee3
EmmanuelR0411 <emmanuelreyna0410@gmail.com>
2022-05-31 17:11:09 -0500

    Correccion de la rotacion y posicion 4

--- localizacion.js
+++ localizacion.js
@@ -20,7 +20,7 @@ window.addEventListener('load', () => {
         const entity = document.createElement('a-entity');
         entity.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
         entity.setAttribute('rotation', '0 0 0');
-        entity.setAttribute('scale', '1 1 1');
+        entity.setAttribute('scale', '1 1 -5');
         // entity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
         entity.setAttribute('position', '0.9 0 -1')
         // entity.setAttribute('animation-mixer', '');
