14a5d842d663121960336ddce86ce9a198773143
EmmanuelR0411 <emmanuelreyna0410@gmail.com>
2022-06-18 16:06:36 -0500

    position

--- localizacion.js
+++ localizacion.js
@@ -1,33 +1,34 @@
-window.addEventListener('load', () => {
-    // const status = document.querySelector('#status');
+// window.addEventListener('load', () => {
+//     // const status = document.querySelector('#status');
 
-    if(navigator.geolocation) {
-        console.log('Locating…')
-        navigator.geolocation.getCurrentPosition(success);
-    } else {
-        console.log('Geolocation is not supported by your browser');
-    }
+//     if(navigator.geolocation) {
+//         console.log('Locating…')
+//         navigator.geolocation.getCurrentPosition(success);
+//     } else {
+//         console.log('Geolocation is not supported by your browser');
+//     }
 
     
 
-    function success(position){
+//     function success(position){
 
-        const scene = document.getElementById('scene');
+//         const scene = document.getElementById('scene');
 
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
 
-        const entity = document.createElement('a-entity');
-        entity.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-        entity.setAttribute('rotation', '0 0 0');
-        entity.setAttribute('scale', '0.15 0.15 0.15');
-        // entity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-        entity.setAttribute('position', { x: -100, y: 0, z: 50 })
+//         const entity = document.createElement('a-entity');
+//         entity.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+//         entity.setAttribute('rotation', '0 180 0');
+//         entity.setAttribute('scale', '0.15 0.15 0.15');
+//         entity.setAttribute('class', 'cantap');
+//         // entity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+//         entity.setAttribute('position', { x: -100, y: 0, z: 50 })
         
-        // entity.setAttribute('animation-mixer', '');
+//         // entity.setAttribute('animation-mixer', '');
 
-        console.log('AAAAAAAAAAAAAAAAAAA: ', latitude,'BBBBBBBBBBBBBBBBBBB: ', longitude)
+//         console.log('AAAAAAAAAAAAAAAAAAA: ', latitude,'BBBBBBBBBBBBBBBBBBB: ', longitude)
 
-        scene.appendChild(entity)
-    }
-})
+//         scene.appendChild(entity)
+//     }
+// })
